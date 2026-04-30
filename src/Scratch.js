ee9ed4a723c6bcd3815b0799be6798811af11a5c
sixaxisjoe67 <sixaxisjoe@gmail.com>
2026-04-30 18:36:03 -0500

    Convert core ActionScript files to JavaScript ES6 modules

---
+++ src/Scratch.js
@@ -0,0 +1,8 @@
+// Scratch.js content converted from ActionScript to JavaScript ES6
+
+export default class Scratch {
+    constructor() {
+        //... properties and logic from ActionScript
+    }
+    //... methods and logic from ActionScript
+}
